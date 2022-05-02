047f52385b3f0067c3a3759f3c3efffc52a3f579
wchester <wchester@ucsd.edu>
2022-05-01 20:53:32 -0700

    CodeFailure

---
+++ SkillDemo1.java
@@ -0,0 +1,6 @@
+public class SkillDemo1{
+    public static String returnSomething(){
+        return "Hello";
+    }
+}
+
---
+++ SkillDemo1Test.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Test{
+    public void returnTest(){
+        assertEquals("Bye", SkillDemo1.returnSomething());
+    }
+}
